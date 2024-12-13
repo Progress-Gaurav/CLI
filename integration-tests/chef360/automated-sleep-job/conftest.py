b2f7da2f42e588cadd62122e457816cfb45f29b1
Progress-Gaurav <gbhandar@progress.com>
2024-12-13 20:39:51 +0530

    Update conftest.py

--- integration-tests/chef360/automated-sleep-job/conftest.py
+++ integration-tests/chef360/automated-sleep-job/conftest.py
@@ -1,16 +1,20 @@
 import pytest
- 
- 
+
+
 def pytest_addoption(parser):
     parser.addoption("--PEM_KEY", action="store", default="default4", help="Value for PEM_KEY")
-    parser.addoption("--NODE_URL", action="store", default="default4", help="Value for NODE_URL")
+    parser.addoption("--NODE_FQDN", action="store", default="default4", help="Value for NODE_FQDN")
     parser.addoption("--LICENSE_ID", action="store", default="default4", help="Value for LICENSE_ID")
- 
- 
- 
+    parser.addoption("--NODE_USER", action="store", default="ubuntu", help="Value for NODE_USER")
+    parser.addoption("--NODE_PORT", action="store", default="22", help="Value for NODE_PORT")
+
+
+
 @pytest.fixture
 def get_env_variables(request):
     env1 = request.config.getoption("--PEM_KEY")
-    env2 = request.config.getoption("--NODE_URL")
+    env2 = request.config.getoption("--NODE_FQDN")
     env3 = request.config.getoption("--LICENSE_ID")
-    return {"PEM_KEY": env1, "NODE_FQDN": env2, "LICENSE_ID": env3}
+    env4 = request.config.getoption("--NODE_USER")
+    env5 = request.config.getoption("--NODE_PORT")
+    return {"PEM_KEY": env1, "NODE_FQDN": env2, "LICENSE_ID": env3, "NODE_USER": env4, "NODE_PORT": env5}
